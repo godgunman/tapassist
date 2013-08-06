82f49d17e2d217942147a332f477aa8b5c548d9f
godgunman <godgunman@gmail.com>
2013-08-07 03:34:15 +0800

    two test case start implement

--- tapassist/src/tw/edu/ntu/csie/mhci/tapassist/MainActivity.java
+++ tapassist/src/tw/edu/ntu/csie/mhci/tapassist/MainActivity.java
@@ -1,26 +1,37 @@
 package tw.edu.ntu.csie.mhci.tapassist;
 
+import tw.edu.ntu.csie.mhci.tapassist.testing.ScrollingCaseActivity;
+import tw.edu.ntu.csie.mhci.tapassist.testing.TappingCaseActivity;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
-import android.widget.ListView;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
-	private ListView listView;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	public void goToTappingTest(View view) {
+		Intent intent = new Intent();
+		intent.setClass(this, TappingCaseActivity.class);
+		startActivity(intent);
+	}
+
+	public void goToScrollingTest(View view) {
+		Intent intent = new Intent();
+		intent.setClass(this, ScrollingCaseActivity.class);
+		startActivity(intent);
+	}
 }
---
+++ tapassist/src/tw/edu/ntu/csie/mhci/tapassist/testing/ScrollingCaseActivity.java
@@ -0,0 +1,14 @@
+package tw.edu.ntu.csie.mhci.tapassist.testing;
+
+import tw.edu.ntu.csie.mhci.tapassist.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScrollingCaseActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_scrolling);
+	}
+
+}
