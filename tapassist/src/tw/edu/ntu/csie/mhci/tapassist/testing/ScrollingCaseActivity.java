b3b3eec94892a039952b7d057b3c7575f90ab791
godgunman <godgunman@gmail.com>
2013-08-09 15:40:47 +0800

    add timer, white space between two tasks

--- tapassist/src/tw/edu/ntu/csie/mhci/tapassist/testing/ScrollingCaseActivity.java
+++ tapassist/src/tw/edu/ntu/csie/mhci/tapassist/testing/ScrollingCaseActivity.java
@@ -8,30 +8,47 @@ import java.util.Map;
 import java.util.Random;
 
 import tw.edu.ntu.csie.mhci.tapassist.R;
+import tw.edu.ntu.csie.mhci.tapassist.utils.Sleep;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ScrollingCaseActivity extends Activity {
 
+	final static long ONE_TASK_TIMEOUT = 60 * 1000;
+	final static long ALL_TASK_TIMEOUT = 300 * 1000;
+	final static long CORRECT_TIME_BOUND = 500;
+
 	private final static int LISTVIEW_SIZE = 30;
+	private final static int[] LISTVIEW_OFFSET = { 0, 400, 800 };
+
+	private RelativeLayout listViewContainerRelativeLayout;
 	private ListView listView;
 	private ImageView redBoxImage;
-	private LinearLayout inforLinearLayout;
 	private TextView taskNumText;
+	private TextView timeCounterText;
 	private View targetListItem;
+
+	private Handler handler = new Handler();
 
 	private int taskNum = 1;
 	private int targetItem;
+
+	private long startTime = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +56,10 @@ public class ScrollingCaseActivity extends Activity {
 		setContentView(R.layout.activity_scrolling);
 
 		redBoxImage = (ImageView) findViewById(R.id.redBoxImage);
-		taskNumText = (TextView) findViewById(R.id.taskNum);
-		inforLinearLayout = (LinearLayout) findViewById(R.id.ll);
+		taskNumText = (TextView) findViewById(R.id.taskNumText);
+		timeCounterText = (TextView) findViewById(R.id.timeCounterText);
+		listViewContainerRelativeLayout = (RelativeLayout) findViewById(R.id.listViewContainer);
+
 		listView = (ListView) findViewById(R.id.listView);
 		listView.setOnScrollListener(new OnScrollListener() {
 
@@ -55,7 +74,8 @@ public class ScrollingCaseActivity extends Activity {
 			}
 		});
 
-		nextRound();
+		createListView();
+		handler.postDelayed(timeCounter, 1000);
 	}
 
 	private boolean checkIfInBox() {
@@ -65,24 +85,61 @@ public class ScrollingCaseActivity extends Activity {
 		if (targetImage == null)
 			return false;
 
-		float y1 = targetListItem.getY() + targetImage.getY()
-				+ inforLinearLayout.getHeight();
+		float y1 = targetListItem.getY() + targetImage.getY();
 		float y2 = y1 + targetImage.getHeight();
 
 		return (redBoxImage.getY() <= y1 && y2 <= redBoxImage.getY()
 				+ redBoxImage.getHeight());
 	}
 
-	private void nextRound() {
+	private void nextRound(final long ms) {
+
+		AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+
+			@Override
+			protected void onPreExecute() {
+				try {
+					Thread.sleep(ms);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				listViewContainerRelativeLayout.setVisibility(View.GONE);
+			}
+
+			@Override
+			protected void onPostExecute(Void result) {
+				createListView();
+			}
+
+			@Override
+			protected Void doInBackground(Void... params) {
+				try {
+					Thread.sleep(1500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				return null;
+			}
+		};
+		task.execute();
+
+	}
+
+	private void createListView() {
+
+		Random random = new Random();
+
+		listViewContainerRelativeLayout.setVisibility(View.VISIBLE);
+		listViewContainerRelativeLayout
+				.setX(LISTVIEW_OFFSET[random.nextInt(3)]);
 		taskNumText.setText("Task : " + taskNum++);
 
 		new Thread(checkCorrect).start();
 		List<Map<String, Integer>> data = new ArrayList<Map<String, Integer>>(
 				LISTVIEW_SIZE);
 
-		targetItem = 5 + Math
-				.abs(new Random().nextInt() % (LISTVIEW_SIZE - 10));
-		Log.d("debug", "targetItem=" + targetItem);
+		targetItem = 5 + random.nextInt(LISTVIEW_SIZE - 10);
+		int selectionItem = targetItem - random.nextInt(5);
 		for (int i = 0; i < LISTVIEW_SIZE; i++) {
 			Map<String, Integer> item = new HashMap<String, Integer>();
 			if (i == targetItem) {
@@ -100,11 +157,13 @@ public class ScrollingCaseActivity extends Activity {
 				R.layout.listview_item, from, to);
 
 		listView.setAdapter(adapter);
+		listView.setSelection(selectionItem);
+
+		Log.d("debug", "targetItem=" + targetItem + ", selectionItem="
+				+ selectionItem);
 	}
 
 	private Runnable checkCorrect = new Runnable() {
-
-		final static long TASK_TIMEOUT = 15 * 1000;
 
 		@Override
 		public void run() {
@@ -112,24 +171,20 @@ public class ScrollingCaseActivity extends Activity {
 			long lastTimeInBox = Long.MAX_VALUE;
 			while (true) {
 
-				if (taskStartTime + TASK_TIMEOUT < new Date().getTime()) {
+				if (taskStartTime + ONE_TASK_TIMEOUT < new Date().getTime()) {
 					runOnUiThread(new Runnable() {
 						@Override
 						public void run() {
 							Toast.makeText(ScrollingCaseActivity.this,
 									"time out", Toast.LENGTH_SHORT).show();
-							nextRound();
+							Sleep.sleep(500);
+							createListView();
 						}
 					});
 					return;
 				}
 
-				try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				Sleep.sleep(100);
 
 				if (targetListItem != null && checkIfInBox() == true) {
 					if (lastTimeInBox == Long.MAX_VALUE) {
@@ -139,17 +194,50 @@ public class ScrollingCaseActivity extends Activity {
 					lastTimeInBox = Long.MAX_VALUE;
 				}
 				long currentTime = new Date().getTime();
-				if (currentTime - lastTimeInBox >= 500) {
+				if (currentTime - lastTimeInBox >= CORRECT_TIME_BOUND) {
 					runOnUiThread(new Runnable() {
 						@Override
 						public void run() {
-							nextRound();
 							Toast.makeText(ScrollingCaseActivity.this,
 									"correct", Toast.LENGTH_SHORT).show();
+							nextRound(500);
 						}
 					});
 					return;
 				}
+			}
+		}
+	};
+
+	private Runnable timeCounter = new Runnable() {
+
+		@Override
+		public void run() {
+			startTime++;
+			timeCounterText.setText(startTime + " seconds");
+			handler.postDelayed(this, 1000);
+
+			if (startTime == ALL_TASK_TIMEOUT) {
+				runOnUiThread(new Runnable() {
+
+					@Override
+					public void run() {
+						AlertDialog.Builder builder = new AlertDialog.Builder(
+								ScrollingCaseActivity.this);
+						builder.setTitle("你已經完成了測試囉!!");
+						builder.setPositiveButton("好", new OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								ScrollingCaseActivity.this.finish();
+							}
+						});
+
+						AlertDialog dialog = builder.create();
+						dialog.show();
+
+					}
+				});
 			}
 		}
 	};
